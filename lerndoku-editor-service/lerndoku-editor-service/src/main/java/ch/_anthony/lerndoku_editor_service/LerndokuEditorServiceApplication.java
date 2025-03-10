271c0f7872fe88fb6cc3443448d6b86b800e5763
Anthony Peña Vargas <anthony.penavargas@lernende.ethz.ch>
2025-03-10 13:52:54 +0000

    create java service project

---
+++ lerndoku-editor-service/lerndoku-editor-service/src/main/java/ch/_anthony/lerndoku_editor_service/LerndokuEditorServiceApplication.java
@@ -0,0 +1,13 @@
+package ch._anthony.lerndoku_editor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LerndokuEditorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LerndokuEditorServiceApplication.class, args);
+	}
+
+}
---
+++ lerndoku-editor-service/lerndoku-editor-service/src/test/java/ch/_anthony/lerndoku_editor_service/LerndokuEditorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch._anthony.lerndoku_editor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LerndokuEditorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
