6cab05ebb25c20d26e063d1723fb3329cf50ee42
Anthony Peña Vargas <anthony.penavargas@lernende.ethz.ch>
2025-03-11 08:57:56 +0000

    add linter with prettier to show formating errors highlighted in code

--- lerndoku-editor-gui/src/app/app.config.ts
+++ lerndoku-editor-gui/src/app/app.config.ts
@@ -3,14 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {
-  provideClientHydration,
-  withEventReplay,
+    provideClientHydration,
+    withEventReplay,
 } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideClientHydration(withEventReplay()),
-  ],
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+        provideClientHydration(withEventReplay()),
+    ],
 };
